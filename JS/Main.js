4347eeeb2e760132187d706cf4756dc42f1cb4af
Teb <Teb@github.com>
2020-11-27 00:48:15 -0600

    v1
    Main, perfil, post, editor, admin

---
+++ JS/Main.js
@@ -0,0 +1,65 @@
+$('.dropdown-submenu > a').on("click", function(e) {
+    var submenu = $(this);
+    $('.dropdown-submenu .dropdown-menu').removeClass('show');
+    submenu.next('.dropdown-menu').addClass('show');
+    e.stopPropagation();
+});
+
+$('.dropdown').on("hidden.bs.dropdown", function() {
+    // hide any open menus when parent closes
+    $('.dropdown-menu.show').removeClass('show');
+});
+
+
+function hideSignLog() 
+{
+    $('#SignLog').hide();
+}
+
+function hideNews() 
+{
+    $('#Edit').hide();
+}
+
+$(document).ready(function(){
+    var submitIcon = $('.searchbox-icon');
+    var inputBox = $('.searchbox-input');
+    var searchBox = $('.searchbox');
+    var isOpen = false;
+    submitIcon.click(function(){
+        if(isOpen == false){
+            searchBox.addClass('searchbox-open');
+            inputBox.focus();
+            isOpen = true;
+        } else {
+            searchBox.removeClass('searchbox-open');
+            inputBox.focusout();
+            isOpen = false;
+        }
+    });  
+     submitIcon.mouseup(function(){
+            return false;
+        });
+    searchBox.mouseup(function(){
+            return false;
+        });
+    $(document).mouseup(function(){
+            if(isOpen == true){
+                $('.searchbox-icon').css('display','block');
+                submitIcon.click();
+            }
+        });
+});
+    function buttonUp()
+    {
+        var inputVal = $('.searchbox-input').val();
+        inputVal = $.trim(inputVal).length;
+        if( inputVal !== 0){
+            $('.searchbox-icon').css('display','none');
+        } else {
+            $('.searchbox-input').val('');
+            $('.searchbox-icon').css('display','block');
+        }
+    }
+
+    
